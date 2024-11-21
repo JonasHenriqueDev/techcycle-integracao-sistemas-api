682f5a419794ab90e62239a02aee82680a61f277
Mateus Almeida <mateusva.1132@gmail.com>
2024-11-21 11:21:43 -0300

    feat: getAllMessages

--- Notificacao/src/main/java/br/upe/is/controller/NotificationController.java
+++ Notificacao/src/main/java/br/upe/is/controller/NotificationController.java
@@ -1,9 +1,12 @@
 package br.upe.is.controller;
 
+import br.upe.is.domain.Notificacao;
 import br.upe.is.service.NotificationConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class NotificationController {
@@ -15,5 +18,11 @@ public class NotificationController {
     public String getLastMessage() {
         String lastMessage = consumer.getLastMessage();
         return lastMessage != null ? lastMessage : "Nenhuma mensagem consumida ainda.";
+    }
+
+    @GetMapping("/consume-all")
+    public List<String> getAllMessages(){
+        List<String> allMessages = consumer.getAllMessages();
+        return allMessages;
     }
 }
--- Notificacao/src/main/java/br/upe/is/service/NotificationConsumer.java
+++ Notificacao/src/main/java/br/upe/is/service/NotificationConsumer.java
@@ -1,19 +1,38 @@
 package br.upe.is.service;
 
+import br.upe.is.domain.Notificacao;
+import br.upe.is.repository.NotificacaoRepository;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Data
 public class NotificationConsumer {
 
     private String lastMessage;
+    private final List<String> allMessages = new ArrayList<>();
+
+    @Autowired
+    NotificacaoRepository notificacaoRepository;
 
     @KafkaListener(topics = "${spring.kafka.topic.name:notification-topic}", groupId = "notification-service")
     public void consume(String message) {
         System.out.println("Received message: " + message);
         this.lastMessage = message;
+        synchronized (allMessages) { // Garantir thread safety para acessos simultâneos
+            allMessages.add(message);
+        }
+    }
+
+    @KafkaListener(topics = "${spring.kafka.topic.name:notification-topic}", groupId = "notification-service")
+    public List<String> getAllMessages() {
+        synchronized (allMessages) { // Retornar uma cópia para evitar modificações externas
+            return new ArrayList<>(allMessages);
+        }
     }
 }
